edd36232c265e70d2221b234111aad0e0509c36c
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2018-02-26 14:02:31 +0100

    :fire: Remove useless things and put the claims in the DecodedDetails of OAuth2Authentication

--- src/main/java/com/example/demo/config/OAuth2SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/OAuth2SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.demo.config;
 
-import com.example.demo.RemoteTokenServices;
+import com.example.demo.KeycloakTokenServices;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,64 +16,40 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableResourceServer
-public class OAuth2SecurityConfiguration extends ResourceServerConfigurerAdapter {
+@EnableAutoConfiguration
+ public class OAuth2SecurityConfiguration extends ResourceServerConfigurerAdapter {
 
+  @Value("${myKeycloak.auth-server-url}")
+  private String authServerUrl;
 
+  @Value("${myKeycloak.realm}")
+  private String realm;
+
+  @Value("${myKeycloak.client-id}")
+  private String clientId;
+
+  @Value("${myKeycloak.client-secret}")
+  private String clientSecret;
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
     http
         .csrf().disable() // disable CSRF to avoid securing all endpoints
         .authorizeRequests()
-        .antMatchers("/customers", "/customers/**").authenticated()
-        .antMatchers("/tests", "/tests/**").authenticated();
-/*
-    http
-        .csrf().disable()
-//        .anonymous().disable()
-        .requestMatcher(authorizationHeaderRequestMatcher())
-        .authorizeRequests()
-//        .antMatchers("/users", "/users/**").permitAll()
-        .antMatchers("/customers", "/customers/**").authenticated()
-        .antMatchers("/tests", "/tests/**").authenticated()
-        .and()
-        .authorizeRequests()
-        .anyRequest().permitAll();
-*/
+        .antMatchers("/customers", "/customers/**").fullyAuthenticated()
+        .antMatchers("/tests", "/tests/**").fullyAuthenticated();
   }
-
-  @Bean
-  @Qualifier("authorizationHeaderRequestMatcher")
-  public RequestMatcher authorizationHeaderRequestMatcher() {
-    return new RequestHeaderRequestMatcher("Authorization");
-  }
-
-//  RemoteTokenServices tokenServices;
-//
-//  @Autowired
-//  @Qualifier("keycloakRemoteTokenServices")
-//  public void setTokenServices(ResourceServerTokenServices tokenServices) {
-//    this.tokenServices = (RemoteTokenServices) tokenServices;
-//    this.tokenServices.setClientId("my-client");
-//    this.tokenServices.setClientSecret("34486f3c-d419-4daf-9308-64fafa9e6dce");
-//    this.tokenServices.setCheckTokenEndpointUrl("http://localhost:32768/auth/realms/demo/protocol/openid-connect/token/introspect");
-//  }
-
 
   @Bean
   public ResourceServerTokenServices tokenServices() {
-    RemoteTokenServices tokenServices = new RemoteTokenServices();
-    tokenServices.setClientId("my-client");
-    tokenServices.setClientSecret("34486f3c-d419-4daf-9308-64fafa9e6dce");
-    tokenServices.setCheckTokenEndpointUrl("http://localhost:32768/auth/realms/demo/protocol/openid-connect/token/introspect");
-    return tokenServices;
+    return new KeycloakTokenServices(authServerUrl, realm, clientId, clientSecret);
   }
 
 
   @Override
   public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-    // this is the 'aud' claim in access_token provided by the AuthorizationServer (Keycloak)
-    resources.resourceId("my-client");
+    // the resourceId is the 'aud' claim in access_token provided by the AuthorizationServer (Keycloak)
+    resources.resourceId(clientId);
     resources.tokenServices(tokenServices());
   }
 }
--- src/main/java/com/example/demo/controller/CustomerApiController.java
+++ src/main/java/com/example/demo/controller/CustomerApiController.java
@@ -1,33 +1,33 @@
 package com.example.demo.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerApiController {
 
-	private static final Logger log = LoggerFactory.getLogger(CustomerApiController.class);
+  private static final Logger log = LoggerFactory.getLogger(CustomerApiController.class);
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<String> getCustomers() {
-		log.info("Context is {}", SecurityContextHolder.getContext());
-		log.info("Authentication is {}", SecurityContextHolder.getContext().getAuthentication());
-		log.info("UserAuthentication is {}", ((OAuth2Authentication)SecurityContextHolder.getContext().getAuthentication()).getUserAuthentication());
-		log.info("Principale is {}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-		log.info("Details is {}", SecurityContextHolder.getContext().getAuthentication().getDetails());
-		log.info("Returning CUSTOMERS list.");
-		return Arrays.asList("Scott Rossillo", "Kyung Lee", "Keith Leggins", "Ben Loy");
-	}
+  @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<String> getCustomers() {
+    log.info("Context is {}", SecurityContextHolder.getContext());
+    log.info("Authentication is {}", SecurityContextHolder.getContext().getAuthentication());
+    log.info("UserAuthentication is {}", ((OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication()).getUserAuthentication());
+    log.info("Principale is {}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    log.info("Details is {}", SecurityContextHolder.getContext().getAuthentication().getDetails());
+    log.info("DecodedDetails is {}", ((OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails()).getDecodedDetails());
+    log.info("Returning CUSTOMERS list.");
+    return Arrays.asList("Scott Rossillo", "Kyung Lee", "Keith Leggins", "Ben Loy");
+  }
 }
